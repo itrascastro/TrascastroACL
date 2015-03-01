c5a4663cbc4127ef3ea7fd0cfb01614149a63dd4
itrascastro <itrascastro@gmail.com>
2015-03-01 15:30:26 +0100

    Module.php updated
    solved module.config.php route problem

--- src/TrascastroACL/Module.php
+++ src/TrascastroACL/Module.php
@@ -19,7 +19,7 @@ class Module implements ConfigProviderInterface, AutoloaderProviderInterface
 {
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../../config/module.config.php';
     }
 
     public function getAutoloaderConfig()
